005d9d9fc8e2db94adc17bacc6a395827b765ddd
antoniosp7 <antoniosalvatperez@gmail.com>
2021-11-12 00:29:20 +0100

    last readme

--- option2/app.py
+++ option2/app.py
@@ -8,26 +8,29 @@ df = pd.read_csv("Dataset-data.csv")
 print('Cantidad de Filas y columnas:',df.shape)
 print('Nombre columnas:',df.columns)
 
-print(df.info())
-
 df = df.drop(['Media Event C','First Offer Date C','Campaign ID','Age C','Partnership Discount Amount C','Payment Plan C','Financing Options C','Financing Option Discount C'],axis=1)
 
+print('INFORMACIÓN GENERAL')
 print(df.info())
-
-print(df['Is Won'].mean())
 
 wins = df[df['Is Won'] == True]
 
 notWins = df[df['Is Won'] == False]
 
+print('INFORMACIÓN GENERAL WINS')
 print(wins.info())
 
+print('INÚMERO DROPS')
 print(wins['Drop'].value_counts())
 
+print('INÚMERO Drop Reason C (wins)')
 print(wins['Drop Reason C'].value_counts())
+print('INÚMERO Lost Deal Reason C (wins)')
 print(wins['Lost Deal Reason C'].value_counts())
 
+print('INÚMERO Lost Deal Reason C (notWins)')
 print(notWins['Lost Deal Reason C'].value_counts())
+print('INÚMERO Lost Reason Notes C (notWins)')
 print(notWins['Lost Reason Notes C'].value_counts())
 
 
